4b85b2e6cf0b990d480fb297f43d5c70c69bd8b7
Dmitry Elfimov <delfimov@1tv.com>
2017-07-25 14:28:47 +0300

    багфиксы

--- scripts/Skeleton.php
+++ scripts/Skeleton.php
@@ -206,16 +206,20 @@ class Skeleton
 
     private static function removeDirectory($dir)
     {
-        $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
-        $files = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::CHILD_FIRST);
-        foreach ($files as $file) {
-            if ($file->isDir()){
-                rmdir($file->getRealPath());
-            } else {
-                unlink($file->getRealPath());
+        if (is_dir($dir)) {
+            $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+            $files = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::CHILD_FIRST);
+            foreach ($files as $file) {
+                if ($file->isDir()) {
+                    rmdir($file->getRealPath());
+                } else {
+                    unlink($file->getRealPath());
+                }
             }
+            rmdir($dir);
+        } elseif (file_exists($dir)) {
+            unlink($dir);
         }
-        rmdir($dir);
     }
 
 }
